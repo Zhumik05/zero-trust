b87e285eb1d6efc955e8cfc92880ab5966468c04
Zhumanazar <zumanazarkulmanov@gmail.com>
2026-04-09 13:25:02 +0000

    added subnet visualization

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+import subprocess
 
 app = Flask(__name__)
 
+def get_clients():
+    output = subprocess.getoutput("ip neigh")
+    clients = []
+ 
+    for line in output.split("\n"):
+        parts = line.split()
+        if len(parts) >= 5:
+            clients.append({ "ip": parts[0], "mac": parts[4] })
+    return clients
+
+
+def get_logs():
+    return subprocess.getoutput("sudo iptables -L")
+
+@app.route("/block/<ip>")
+def block_ip(ip):
+    subprocess.call("sudo iptables -A INPUT -s {ip} -j DROP", shell=True)
+    return redirect("/")
+
+@app.route("/allow/<ip>")
+def allow_ip(ip):
+    subprocess.call("sudo iptables -A INPUT -s {ip} -j ACCEPT", shell=True)
+    return redirect("/")
+
 @app.route("/")
-def home():
-   return "Hello!"
+def dashboard():
+    clients = get_clients()
+    logs = get_logs()
+  
+    return render_template("index2.html", clients=clients, logs=logs, blocked=blocked)
+
+def get_blocked_count():
+    rules = subprocess.getoutput("sudo iptables -L")
+    return rules.count("DROP")
 
 if __name__ == "__main__":
-   app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
